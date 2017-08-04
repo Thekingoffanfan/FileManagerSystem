a9a7e94537d685667a6903d3f0352637cca8c2f0
lixiang <lixstudy@163.com>
2017-08-04 17:11:24 +0800

    过滤器

--- WebContent/main.jsp
+++ WebContent/main.jsp
@@ -10,24 +10,33 @@
 body {
 	text-align: center
 }
+
 h1 {
 	color: red
 }
+
 a {
-  color:blue;
+	color: blue;
 }
 </style>
 </head>
 <body>
+	<%
+		String filterFeedback = (String)request.getAttribute("filterFeedback");
+	if(filterFeedback != null){%>
+		<script>alert('<%=filterFeedback%>')</script>
+	<%}%>
 	<h1>文档管理系统登录界面</h1>
 	<h2>用户登录</h2>
 	<form action="Login" method="post">
 		用户名：<input type="text" id="userName" name="userName" size="20">
-		<br /> 密 &nbsp;&nbsp;码：<input type="password" id=userPassword name="userPassword" 
-		maxlength="20" size="20" > <br /> <br />
-			 <input type="submit"  name="logIn" id="logIn" value="登录"> 
-			 <input type="button" onclick="window.location.href='register.jsp'" onclick="openpage()" value="注册">
-	</form><br/>
-	<a href='startPage.jsp'>导航页</a>	
-	</body>
+		<br /> 密 &nbsp;&nbsp;码：<input type="password" id=userPassword
+			name="userPassword" maxlength="20" size="20"> <br /> <br />
+		<input type="submit" name="logIn" id="logIn" value="登录"> <input
+			type="button" onclick="window.location.href='register.jsp'"
+			onclick="openpage()" value="注册">
+	</form>
+	<br />
+	<a href='startPage.jsp'>导航页</a>
+</body>
 </html>
--- src/com/huanke/filter/SystemFilter.java
+++ src/com/huanke/filter/SystemFilter.java
@@ -8,6 +8,9 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet Filter implementation class SystemFilter
@@ -31,14 +34,41 @@ public class SystemFilter implements Filter {
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		// place your code here
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
+		HttpSession session = request.getSession();
 
-		System.out.println("进行过滤操作......");
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
+		// 判断用户访问的是登录操作，不进行自动登录，将jsp文件名拆截出来
+		String uri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String path = uri.substring(contextPath.length());
+		//
+		// System.out.println(uri);
+		// System.out.println(contextPath);
+		// System.out.println(path);
+		// 如果是登录或者注册界面就放行，否则检测用户是否登录
+		if (path.equals("/main.jsp") || path.equals("/register.jsp")) {
+			System.out.println("登录或注册界面");
+			chain.doFilter(request, response);
+			return;
+		} else {
+			String userName = (String) session.getAttribute("userInfo");
+
+			// 用户信息已存在说明已经登录
+			if (userName != null) {
+				System.out.println("已登录");
+				chain.doFilter(request, response);
+				return;
+			} else {
+				System.out.println("未登录");
+				request.setAttribute("filterFeedback", "用户未登录，或登录已过期，请重新登录");
+				request.getRequestDispatcher("/main.jsp").forward(request, response);
+			}
+		}
 	}
 
 	/**
